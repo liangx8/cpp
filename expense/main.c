1f7f76ac6dda50b9ff4e2fe4d0caa13ae4b49ee7
Ram <ram@acer-laptop.pr>
2026-03-17 16:57:26 +0800

    expense progress

--- expense/main.c
+++ expense/main.c
@@ -3,11 +3,20 @@
 #include <locale.h>
 #include <sqlite3.h>
 #include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
+char buf[256];
+void foreach(void *sb);
 void *read_lines(const char*);
 int main(int argc,char **argv)
 {
     sqlite3 *db;
     setlocale(LC_ALL,"");
+    const char *home=getenv("HOME");
+    
+    strcpy(buf,home);
+    strcat(buf,"/test.db");
+    wprintf(L"数据库文件:%s\n",buf);
     int res=sqlite3_open("/home/cyc/test.db",&db);
     if (res==SQLITE_OK)
         wprintf(L"ＳＱＬＩＴＥ３ open result code (%d)\n",res);
@@ -15,6 +24,7 @@ int main(int argc,char **argv)
     if(argc>1){
         void *obj=read_lines(argv[1]);
         if(obj!=NULL){
+            foreach(obj);
             free(obj);
             wprintf(L"释放内存\n");
         }
--- expense/string_buffer.c
+++ expense/string_buffer.c
@@ -14,7 +14,7 @@ void *read_lines(const char *fname)
         return NULL;
     }
     fseek(hdl,0,SEEK_END);
-    long size=ftell(hdl);
+    long size=ftell(hdl)+1;
     wprintf(L"size:%ld\n",size);
     rewind(hdl);
     int max_linesize=size/4;
@@ -30,17 +30,36 @@ void *read_lines(const char *fname)
     fread(sb->buf,1,size,hdl);
     fclose(hdl);
     *(sb->strs)=sb->buf;
-    char *sptr=sb->buf;
+    char *sptr;
     const char **ptrs=sb->strs+1;
+    int cnt=1;
+    int str_begin=0;
     for(int ix=0;ix<size;ix++){
-        int str_begin=0;
-        while(1){
-            if(*sptr=='\n' || *sptr=='\r'){
-                *sptr='\0';
-                sptr++;
-                str_begin=1;
-                continue;
-            }
+        sptr=sb->buf+ix;
+        if(*sptr=='\n' || *sptr=='\r'){
+            *sptr='\0';
+            str_begin=1;
+            continue;
+        }
+        if(str_begin){
+            str_begin=0;
+            *ptrs=sptr;
+            ptrs++;
+            cnt++;
+        }
     }
+    sptr=sb->buf+size;
+    *sptr='\0';
+    // sb->buf[size]='\0';
+    sb->strs_size=cnt;
     return base;
 }
+void foreach(struct StringBuffer *sb)
+{
+    int cnt=0;
+    while(cnt < sb->strs_size){
+        wprintf(L"%3d %s\n",cnt,*(sb->strs+cnt));
+        cnt ++;
+    }
+}
+
